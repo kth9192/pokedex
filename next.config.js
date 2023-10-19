bcb9547a3be24de12aa897d19e01d9cda7e9fa6b
kth919 <kth9193@gmail.com>
2023-10-19 16:07:55 +0900

    :construction: production 로그 삭제

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ['i.scdn.co', 'raw.githubusercontent.com'],
   },
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
 };
 
 module.exports = nextConfig;
